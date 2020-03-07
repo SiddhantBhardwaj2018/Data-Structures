4930120e96373205221c47376292fa3816690488
Siddhant Bhardwaj <44534839+SiddhantBhardwaj2018@users.noreply.github.com>
2020-03-07 06:31:28 -0700

    Add files via upload

---
+++ Bubble Sort.py
@@ -0,0 +1,14 @@
+
+def bubbleSort(lst):
+    '''
+    '''
+    n = len(lst)
+    for i in range(1,n):
+        for j in range(0, n-1):
+            key = lst[j + 1]
+            if lst[j] > lst[j + 1]:
+                lst[j + 1] = lst[j]
+                lst[j] = key
+    return lst
+    
+print(bubbleSort([5,2,1,4,6,3]))

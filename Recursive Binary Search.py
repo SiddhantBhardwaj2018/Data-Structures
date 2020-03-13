2737facf31d75ad53beff9a08de0989eb45336a1
Siddhant Bhardwaj <44534839+SiddhantBhardwaj2018@users.noreply.github.com>
2020-03-13 14:29:00 -0700

    Add files via upload

---
+++ Recursive Binary Search.py
@@ -0,0 +1,16 @@
+
+def binary_search_recursive(data,target,low,high):
+    '''
+    Recursive implementation of binary search algorithm
+    '''
+    if low > high:
+        return False
+    else:
+        mid = (low + high) // 2
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            return binary_search_recursive(data,target,low,mid - 1)
+        else:
+            return binary_search_recursive(data,target,mid + 1,high)
+            
